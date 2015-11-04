75e6f5fdfca5c2f14740c9635f3de0ee0acb2fa3
sarath-jiguru <sarath@adnear.com>
2015-11-04 09:57:08 +0530

    twitter api test cases

--- scrape/test_twitterSearch.py
+++ scrape/test_twitterSearch.py
@@ -1,8 +1,13 @@
 from unittest import TestCase
+from domain.Listing import Listing
+from scrape.TwitterSearch import TwitterSearch
 
 __author__ = 'sarath'
 
 
 class TestTwitterSearch(TestCase):
+    def setUp(self):
+        self.twitter = TwitterSearch()
+
     def test_search_show(self):
-        self.fail()
+        print(self.twitter.search_show(Listing("The Wendy Williams Show|tv")))
