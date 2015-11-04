53b25015d2904895b7378e83b7c6e3a04725e052
sarath-jiguru <sarath@adnear.com>
2015-11-04 09:56:37 +0530

    Facebook api handler

--- scrape/FbSearch.py
+++ scrape/FbSearch.py
@@ -1,1 +1,13 @@
+"""FbSearch: This class is designed to search for a tv listing(as of now a tv show, or a movie
+ and get its official Facebook page, likes count etc if ever exists
+"""
+
 __author__ = 'sarath'
+
+import facebook
+
+
+class FbSearch():
+    def __init__(self):
+        self.graph_api = facebook.GraphAPI
+        self.request = self.graph_api.request('/search', {'q': 'parenthood', 'type': 'page', 'fields': 'likes,id,name'})
